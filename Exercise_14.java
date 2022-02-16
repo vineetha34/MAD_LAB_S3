1826054f800a35235981bd55abb844acd19b5eed
vineetha34 <75126810+vineetha34@users.noreply.github.com>
2022-02-16 15:32:32 +0530

    Exercise_14.java

---
+++ Exercise_14.java
@@ -0,0 +1,50 @@
+#first.java
+package com.example.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn1;
+        btn1=(Button) findViewById(R.id.btn_1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
+    }
+}
+
+
+#second.java
+package com.example.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toast.makeText(this,"Welcome",Toast.LENGTH_SHORT).show();
+    }
+}
